8191c6bddb6067933116b129510b27cd12cfc7e1
MohamedArshadGit <123241512+MohamedArshadGit@users.noreply.github.com>
2023-11-27 12:35:46 +0530

    test_train.py

--- models/test_train.py
+++ models/test_train.py
@@ -20,22 +20,20 @@ labels.extend(['Uninfected'] * len(Uninfected_images))
 file_paths.extend([os.path.join('Parasitized', img) for img in Parasitized_images])
 labels.extend(['Parasitized'] * len(Parasitized_images))
 
-# Split the data into train and test sets (80% train, 30% test)
+# Split the data into train and test sets (80% train, 20% test)
 train_files, test_files, train_labels, test_labels = train_test_split(file_paths, labels, test_size=0.20, random_state=42)
 
 # Create train and test directories
 train_directory = r"c:\Users\Mohamed Arshad\Desktop\cell_images\train"
 test_directory = r"C:\Users\Mohamed Arshad\Desktop\cell_images\test"
 
-os.makedirs(train_directory, exist_ok=True) #When exist_ok=True, it means that the function will not raise an error if the directory already exists; instead, it will silently ignore the creation attempt.
+os.makedirs(train_directory, exist_ok=True) 
 os.makedirs(test_directory, exist_ok=True)
 
 # Move train files to train directory
 for file, label in zip(train_files, train_labels):
-    src = os.path.join(main_directory, file)#Constructs the source path by joining the main_directory with the file name.
-    dst = os.path.join(train_directory, label, os.path.basename(file)) #is used to extract filename from file path os.path.basename()
-    #is a function in Python's os.path module that extracts the filename from a path. It returns the final component of a path,
-    #which represents the actual filename or the last part of the path.eg:example.txt from the path /home/user/Documents/example.txt.
+    src = os.path.join(main_directory, file)
+    dst = os.path.join(train_directory, label, os.path.basename(file)) 
     os.makedirs(os.path.dirname(dst), exist_ok=True)
     os.replace(src, dst)
 
